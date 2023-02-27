ceaefa97fc99fd192896f0814a3d2de7dcfaec9d
Yuhei Jotaki <yuheijotaki@gmail.com>
2023-02-27 10:09:17 +0900

    add next/font module

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,34 @@ Router.events.on('routeChangeStart', () => NProgress.start())
 Router.events.on('routeChangeComplete', () => NProgress.done())
 Router.events.on('routeChangeError', () => NProgress.done())
 
+// local Fonts
+import localFont from '@next/font/local'
+const PostgroteskBookFont = localFont({
+  src: [
+    {
+      path: './fonts/PostGrotesk-Book.woff',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './fonts/PostGrotesk-Book.eot',
+      weight: '400',
+      style: 'normal',
+    }
+  ],
+  display: 'swap',
+  preload: true
+})
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        :root {
+          --postgrotesk-book: ${PostgroteskBookFont.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  )
 }
